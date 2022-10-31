a820d9cf7084e07bdc05861e0920201cef5eea18
Gloria <ga365743@gmail.com>
2022-10-30 18:18:16 -0600

    about

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,8 @@ module.exports = {
         lime: 'rgb(var(--lime-green) / <alpha-value>)',
         mindaro: 'rgb(var(--mindaro) / <alpha-value>)',
         teal: 'rgb(var(--teal) / <alpha-value>)',
-        cream: '#FCFFD2',
+        astronaut: 'rgb(var(--astronaut) / <alpha-value>)',
+        nara: 'rgb(var(--nara) / <alpha-value>)',
       },
       fontFamily: {
         title: ['yipes', 'CascadiaCode', 'sans-serif'],
@@ -19,6 +20,7 @@ module.exports = {
     },
     backgroundImage: {
       grid: "url('/assets/lines.svg')",
+      ONE: "url('/assets/oracle-one-logo.svg')",
     },
   },
   plugins: [
@@ -26,13 +28,12 @@ module.exports = {
       addUtilities({
         '.bg-radial-gradient': {
           'background-image': `
-            radial-gradient(farthest-side at 4% 4%, #09BAD5 0%, rgba(0, 0, 0, 0) 80%),
-            radial-gradient(circle farthest-side at 0% 100%, #25ED91 0%, #EEF88E);`,
+            radial-gradient(farthest-side at 4% 4%, #4376EC 0%, rgba(0, 0, 0, 0) 80%),
+            radial-gradient(farthest-side at 0% 100%, #FFEBD0 0%, #EEF88E);`,
         },
         '.mesh-gradient': {
           'background-image': `
-            radial-gradient(farthest-side at 89% 9%, #EEF88E 0%,rgba(0, 0, 0, 0) 70%), radial-gradient(farthest-side at 4% 45%, #09BAD5 0%, rgba(0, 0, 0, 0) 80%),
-            radial-gradient(circle farthest-side at 35% 26%, #25ED91 0%, transparent);`,
+            radial-gradient(farthest-side at 89% 9%, #19a5979d, rgba(0, 0, 0, 0) 70%), radial-gradient(farthest-side at 4% 45%, #4376ecbe, rgba(0, 0, 0, 0) 80%), radial-gradient(farthest-side at 35% 26%, #FFF3D9 0%, transparent);`,
         },
         '.vertical': {
           'writing-mode': 'vertical-rl',
